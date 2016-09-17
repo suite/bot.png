6881e8f250e59618e964a07f115210435b1f081b
Matt <gr8bitmail@gmail.com>
2016-09-16 23:31:39 -0500

    Update config.js

--- config.js
+++ config.js
@@ -5,8 +5,8 @@
 // var config = require('./config.js');
 
 module.exports = {
-  consumer_key:         'YYdaA3wEHGGWtLoBh1j9I3i3M',
-  consumer_secret:      'Q1qYaF724uCaGwga5LCWBeKHXufiUgXtNFl4JKN3a3kmiYHXKl',
-  access_token:         '776262632819097600-xRVeDwJrqqjZahQIzvojdL178au2YHF',
-  access_token_secret:  'YUvapcugLSdOFRNjWtaz2oKn7zXTSXFvEYLxYVRzJMv0w'
+  consumer_key:         'key',
+  consumer_secret:      'key',
+  access_token:         'key',
+  access_token_secret:  'key'
 }
